cbdc94b03452799550eb471a677182561678ef99
Mykhaylo Pavlov <info@coddy.ca>
2022-02-09 11:08:34 -0500

    add options to chrome

--- moodle/moodle_methods.py
+++ moodle/moodle_methods.py
@@ -2,12 +2,24 @@ import sys
 import datetime
 import moodle_locators as locators
 from time import sleep
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
-driver = webdriver.Chrome('/var/lib/jenkins/workspace/PythonSeleniumMoodle/moodle/chromedriver')
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
 driver.implicitly_wait(30)
 driver.maximize_window()
 

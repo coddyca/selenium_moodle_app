3835237aa2b877ac7006f6e350d40c8aa3e4cbd3
Mykhaylo Pavlov <info@coddy.ca>
2022-02-08 20:10:34 -0500

    Fix moodle methods file

--- moodle/moodle_methods.py
+++ moodle/moodle_methods.py
@@ -1,18 +1,13 @@
 import sys
-import pytest
 import datetime
 import moodle_locators as locators
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 
-s = Service(executable_path='./chromedriver')
-
-
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(30)
 driver.maximize_window()
 
